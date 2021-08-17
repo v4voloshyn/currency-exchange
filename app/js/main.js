67261024e457f1885bd2289d1c18b1edffa5583c
Voloshyn <v4voloshyn@gmail.com>
2021-08-17 15:13:39 +0300

    splited branch

--- app/js/main.js
+++ app/js/main.js
@@ -5,55 +5,67 @@
 //    console.log(data);
 // })
 
+
+// Снять Дизаблед ярлык и разрешить реконвертировать валюты между собой
+// Подставлять валюты с базы даных
+let rubValue;
+let rubPrevious;
+//Объект с курсами валют
+
 const rates = {
    "RUB": {
       "ID": "0000",
       "NumCode": "0000",
       "CharCode": "RUB",
-      "Nominal": 1,
+      "Nominal": 100 ,
       "Name": "Росийский рубль",
-      "Value": "rubValue",
-      "Previous": "rubPrevious",
-      }
+      "Value": rubValue,
+      "Previous": rubPrevious
    }
-
-   const inputField = document.querySelector('#input');
-   const resultField = document.querySelector('#result');
-   const selectField = document.querySelector('#select');
-   const outputValue = selectField.value;
-   let inputCurrencyName = document.querySelector('option').getAttribute('data-input-value'); //'UAH' по умолчанию
-   console.log(inputCurrencyName);
-   let outputCurrencyName = document.querySelector('option').getAttribute('value'); 
-   console.log(outputCurrencyName);
+}
 
 
-   const elementUSD = document.querySelector('[data-value="USD"]');
-   const elementEUR = document.querySelector('[data-value="EUR"]');
-   const elementGBP = document.querySelector('[data-value="GBP"]');
-   
+
+const inputField = document.querySelector('#input');
+const resultField = document.querySelector('#result');
+const selectField = document.querySelector('#select');
+
+let inputCurrencyName = document.querySelector('option').getAttribute('data-input-value'); //'UAH' по умолчанию
+console.log(inputCurrencyName);
+// let outputCurrencyName = document.querySelector('option').getAttribute('value'); 
+// console.log(outputCurrencyName);
+
+const elementUSD = document.querySelector('[data-value="USD"]');
+const elementEUR = document.querySelector('[data-value="EUR"]');
+const elementGBP = document.querySelector('[data-value="GBP"]');
+
 getCurrencies();
 
 
-async function getCurrencies()   {
+async function getCurrencies() {
    const response = await fetch('https://www.cbr-xml-daily.ru/daily_json.js');
    const result = await response.json();
    const data = await result;
+
    const rubValue = data.Valute.UAH.Value / data.Valute.UAH.Nominal;//Вычисление значений рубля отталкиваясь от курса ГРН
    const rubPrevious = data.Valute.UAH.Previous / data.Valute.UAH.Nominal;
 
-   rates.UAH = data.Valute.UAH;
 
    rates.RUB.Value = rubValue;//Установка значений рубля отталкиваясь от курса ГРН
    rates.RUB.Previous = rubPrevious;
 
+   rates.UAH = data.Valute.UAH;
    rates.USD = data.Valute.USD;
    rates.EUR = data.Valute.EUR;
    rates.GBP = data.Valute.GBP;
    rates.TRY = data.Valute.TRY;
+   console.log(rates.RUB);
+   console.log(rates.UAH);
    console.log(rates.USD);
-   elementUSD.textContent = (rates.USD.Value / rubValue ).toFixed(2);
-   elementGBP.textContent = (rates.GBP.Value / rubValue ).toFixed(2);
-   elementEUR.textContent = (rates.EUR.Value/ rubValue ).toFixed(2);
+
+   elementUSD.textContent = (rates.USD.Value / rates.UAH.Value * rates.UAH.Nominal).toFixed(2);
+   elementGBP.textContent = (rates.GBP.Value / rates.UAH.Value * rates.UAH.Nominal).toFixed(2);
+   elementEUR.textContent = (rates.EUR.Value / rates.UAH.Value * rates.UAH.Nominal).toFixed(2);
    //Цвет для информера USD
    if (rates.USD.Value > rates.USD.Previous) {
       elementUSD.classList.add('top');
@@ -76,15 +88,14 @@ async function getCurrencies()   {
    }
 }
 
+function convertValue() {
+   resultField.value =
+      ((parseFloat(inputField.value) * rates[inputCurrencyName].Value / rates[inputCurrencyName].Nominal) /
+         (rates[select.value].Value / rates[select.value].Nominal)).toFixed(4);
+}
+inputField.oninput = convertValue;
+selectField.oninput = convertValue;
 
-inputField.oninput = function() {
-   resultField.value = (parseFloat(inputField.value) * rates.RUB.Value / (rates[select.value].Value / rates[select.value].Nominal)).toFixed(2);
-   console.log(rates.USD.Value);
-}
-select.oninput = function() {
-   resultField.value = (parseFloat(inputField.value) * rates.RUB.Value / (rates[select.value].Value / rates[select.value].Nominal)).toFixed(2);
-   console.log(rates.USD.Value);
-}
 
 // console.log(input);
 
